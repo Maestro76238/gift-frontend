67cf07ef64f5b57b78954ca61fb53ff75735a26a
aleksandr <ksalow777@gmail.com>
2025-12-19 18:51:36 +0300

    ZAEBALO

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,13 +1,22 @@
 const API_URL = "https://gift-backend-tn9w.onrender.com";
 
 export async function getGift(code) {
-  const res = await fetch(`${API_URL}/api/get-gift/${code}`, {
-    method: "GET",
-  });
+  const res = await fetch(
+    `${API_URL}/api/check-gift`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ code }),
+    }
+  );
+
+  const data = await res.json();
 
   if (!res.ok) {
-    throw new Error("Invalid or used code");
+    throw new Error(data.error || "Invalid or used code");
   }
 
-  return await res.json();
+  return data;
 }
